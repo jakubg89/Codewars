e8913c03c8b9392b7c1acd7d874b205ca15191bc
jakubg89 <114975326+jakubg89@users.noreply.github.com>
2022-10-24 13:23:56 +0200

    First element of list

--- 102022/First elements.py
+++ 102022/First elements.py
@@ -1,4 +1,6 @@
-Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+"""
+Write a function to get the first element(s) of a sequence.
+Passing a parameter n (default=1) will return the first n element(s) of the sequence.
 
 If n == 0 return an empty sequence []
 
@@ -8,3 +10,12 @@ first(arr)    # --> ['a']
 first(arr, 2) # --> ['a', 'b']
 first(arr, 3) # --> ['a', 'b', 'c']
 first(arr, 0) # --> []
+"""
+
+
+def first(seq, n=1):
+    return seq[:n]
+
+
+arr = ['a', 'b', 'c', 'd', 'e']
+print(first(arr, 2))
